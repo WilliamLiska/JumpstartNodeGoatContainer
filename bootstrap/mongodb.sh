cf27d10a3214581201beafe9a8db048bef767471
William Liska <william.liska@gmail.com>
2017-05-07 11:49:08 -0400

    Switched to a 32 bit VM and mongo version to enable nested virtualization.

--- bootstrap/mongodb.sh
+++ bootstrap/mongodb.sh
@@ -5,4 +5,4 @@ echo "deb http://repo.mongodb.org/apt/ubuntu precise/mongodb-org/3.2 multiverse"
 #Step 3: Refresh the local database with the packages
 sudo apt-get update
 #Step 4: Install the last stable MongoDB version and all the necessary packages on our system
-sudo apt-get install -y mongodb-org
+sudo apt-get install -y mongodb
